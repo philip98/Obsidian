87015ada281d17b2337f3510f8c8e5ea662a22f7
Philip Schlösser <schloesph@web.de>
2015-05-13 08:23:48 +0200

    Fix export functionality
    I changed the export method so that the correct form is
    displayed (and not the address of the combo box).
    Furthermore, I added projection to the SQL queries
    because appearently QSqlQuery doesn't handle multiple
    tables together with SELECT * ... so well. Now, only the
    names of the students and the books' titles will be
    fetched.

--- src/listview.cpp
+++ src/listview.cpp
@@ -403,9 +403,9 @@ void ListView::toggle() {
  * \brief Wird bei Klick auf Bearbeiten|Exportieren aufgerufen
  */
 void ListView::exportLendings() {
-	QString fname;
-	if ((fname = QFileDialog::getSaveFileName(this, tr("Ausleihen exportieren"),
-						  QString(), "Textdateien (*.txt)")).isEmpty())
+	QString fname = QFileDialog::getSaveFileName(this, tr("Ausleihen exportieren"),
+						     QDir::homePath(), "Textdateien (*.txt)");
+	if (fname.isEmpty())
 		return;
 	QFile file{fname};
 	QSqlRecord record;
@@ -414,21 +414,21 @@ void ListView::exportLendings() {
 	if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
 		return;
 
-	stream << "Sonstige ausstehende Ausleihen (" << a_form << ")\n\n" << qSetPadChar('_');
-	a_q.prepare("SELECT * FROM (SSchueler LEFT JOIN sausleihe ON SSchueler.id = sausleihe.sid) LEFT JOIN Buch ON sausleihe.bid = Buch.isbn WHERE `Klasse` = :klasse ORDER BY SSchueler.Name");
+	stream << "Sonstige ausstehende Ausleihen (" << a_tableModel->form() << ")\n\n" << qSetPadChar('.');
+	a_q.prepare("SELECT SSchueler.name AS name, Buch.titel AS titel FROM (SSchueler LEFT JOIN sausleihe ON SSchueler.id = sausleihe.sid) LEFT JOIN Buch ON sausleihe.bid = Buch.isbn WHERE `Klasse` = :klasse ORDER BY SSchueler.Name");
 	a_q.bindValue(":klasse", a_tableModel->form());
 	if (!::exec_first(&a_q)) return;
 	do {
 		record = a_q.record();
-		stream << qSetFieldWidth(30) << record.value("SSchueler.Name").toString() << qSetFieldWidth(40) << record.value("Buch.titel").toString() << endl;
+		stream << qSetFieldWidth(30) << record.value("name").toString() << qSetFieldWidth(40) << record.value("titel").toString() << endl;
 	} while (a_q.next());
 
 	stream << "\n\n\nNicht zurückgegebene Bücher (" << a_form << ")\n\n";
-	a_q.prepare("SELECT * FROM (SSchueler LEFT JOIN btausch ON SSchueler.id = btausch.sid) LEFT JOIN Buch ON btausch.bid = Buch.isbn WHERE Klasse = :klasse ORDER BY SSchueler.Name");
+	a_q.prepare("SELECT SSchueler.name AS name, Buch.titel AS titel FROM (SSchueler LEFT JOIN btausch ON SSchueler.id = btausch.sid) LEFT JOIN Buch ON btausch.bid = Buch.isbn WHERE SSchueler.Klasse = :klasse ORDER BY SSchueler.Name");
 	a_q.bindValue(":klasse", a_tableModel->form());
 	if(!::exec_first(&a_q)) return;
 	do {
 		record = a_q.record();
-		stream << qSetFieldWidth(30) << record.value("SSchueler.name").toString() << qSetFieldWidth(40) << record.value("Buch.titel").toString() << endl;
+		stream << qSetFieldWidth(30) << record.value("name").toString() << qSetFieldWidth(40) << record.value("titel").toString() << endl;
 	} while (a_q.next());
 }
